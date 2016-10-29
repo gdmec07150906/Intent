b322e07825e012bda1c73603c5465177e23382c9
gdmec07150906 <609482712@qq.com>
2016-10-29 23:49:57 +0800

    Initial commit

---
+++ app/src/main/java/com/example/windows/intent/IntentDemo3.java
@@ -0,0 +1,14 @@
+package com.example.windows.intent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by windows on 2016/10/29.
+ */
+public class IntentDemo3 extends AppCompatActivity{
+    protected void Oncreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_intent_demo3);
+    }
+}
